de30370371e7b1bc5fc676d3b4ccb8c774f90106
FreezingTexture22 <andreisesiunin@gmail.com>
2024-07-29 18:49:17 +0200

    update

---
+++ src/ru/andreisesiunin/Case1.java
@@ -0,0 +1,18 @@
+package ru.andreisesiunin;
+
+import java.util.Arrays;
+
+public class Case1 {
+    public static void main(String[] args) {
+        int[] a = {5, 5};
+        int b = 1;
+
+        a[b] = b = 0;
+
+        //what will print this code?
+        System.out.println(b);
+        System.out.println(b);
+        System.out.println(b);
+        System.out.println(Arrays.toString(a));
+    }
+}
